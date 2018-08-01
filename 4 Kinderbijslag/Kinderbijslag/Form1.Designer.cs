e2e205705adad084108f1bf89e4712b6bccf0ab0
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-08-01 15:23:40 +0200

    Add files via upload
    Kinderbijslag

---
+++ 4 Kinderbijslag/Kinderbijslag/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Kinderbijslag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbGeboorteDatum = new System.Windows.Forms.TextBox();
+            this.tbKinderBijslag = new System.Windows.Forms.TextBox();
+            this.btnKinderBijslag = new System.Windows.Forms.Button();
+            this.lblGeboorteDatum = new System.Windows.Forms.Label();
+            this.btnInvoeren = new System.Windows.Forms.Button();
+            this.lblAantal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbGeboorteDatum
+            // 
+            this.tbGeboorteDatum.Location = new System.Drawing.Point(98, 24);
+            this.tbGeboorteDatum.Name = "tbGeboorteDatum";
+            this.tbGeboorteDatum.Size = new System.Drawing.Size(100, 20);
+            this.tbGeboorteDatum.TabIndex = 0;
+            // 
+            // tbKinderBijslag
+            // 
+            this.tbKinderBijslag.Location = new System.Drawing.Point(98, 143);
+            this.tbKinderBijslag.Name = "tbKinderBijslag";
+            this.tbKinderBijslag.Size = new System.Drawing.Size(100, 20);
+            this.tbKinderBijslag.TabIndex = 1;
+            // 
+            // btnKinderBijslag
+            // 
+            this.btnKinderBijslag.Location = new System.Drawing.Point(98, 114);
+            this.btnKinderBijslag.Name = "btnKinderBijslag";
+            this.btnKinderBijslag.Size = new System.Drawing.Size(100, 23);
+            this.btnKinderBijslag.TabIndex = 2;
+            this.btnKinderBijslag.Text = "Kinderbijslag";
+            this.btnKinderBijslag.UseVisualStyleBackColor = true;
+            this.btnKinderBijslag.Click += new System.EventHandler(this.btnKinderBijslag_Click);
+            // 
+            // lblGeboorteDatum
+            // 
+            this.lblGeboorteDatum.AutoSize = true;
+            this.lblGeboorteDatum.Location = new System.Drawing.Point(12, 27);
+            this.lblGeboorteDatum.Name = "lblGeboorteDatum";
+            this.lblGeboorteDatum.Size = new System.Drawing.Size(80, 13);
+            this.lblGeboorteDatum.TabIndex = 3;
+            this.lblGeboorteDatum.Text = "Geboortedatum";
+            // 
+            // btnInvoeren
+            // 
+            this.btnInvoeren.Location = new System.Drawing.Point(98, 50);
+            this.btnInvoeren.Name = "btnInvoeren";
+            this.btnInvoeren.Size = new System.Drawing.Size(100, 23);
+            this.btnInvoeren.TabIndex = 4;
+            this.btnInvoeren.Text = "Invoeren";
+            this.btnInvoeren.UseVisualStyleBackColor = true;
+            this.btnInvoeren.Click += new System.EventHandler(this.btnInvoeren_Click);
+            // 
+            // lblAantal
+            // 
+            this.lblAantal.AutoSize = true;
+            this.lblAantal.Location = new System.Drawing.Point(204, 27);
+            this.lblAantal.Name = "lblAantal";
+            this.lblAantal.Size = new System.Drawing.Size(13, 13);
+            this.lblAantal.TabIndex = 5;
+            this.lblAantal.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 245);
+            this.Controls.Add(this.lblAantal);
+            this.Controls.Add(this.btnInvoeren);
+            this.Controls.Add(this.lblGeboorteDatum);
+            this.Controls.Add(this.btnKinderBijslag);
+            this.Controls.Add(this.tbKinderBijslag);
+            this.Controls.Add(this.tbGeboorteDatum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbGeboorteDatum;
+        private System.Windows.Forms.TextBox tbKinderBijslag;
+        private System.Windows.Forms.Button btnKinderBijslag;
+        private System.Windows.Forms.Label lblGeboorteDatum;
+        private System.Windows.Forms.Button btnInvoeren;
+        private System.Windows.Forms.Label lblAantal;
+    }
+}
+
---
+++ 4 Kinderbijslag/Kinderbijslag/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kinderbijslag
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Declareren van de variabelen
+        DateTime[] arrayGeboorteDatum = new DateTime[20];
+        int intAantal = 0, intTeller, intLeefTijd;
+        double dblKinderBijslag;
+
+        private void btnKinderBijslag_Click(object sender, EventArgs e)
+        {
+            for (intTeller = 1; intTeller <= intAantal; intTeller++)
+            {
+                //Leeftijd bepalen
+                intLeefTijd = Convert.ToInt16(DateTime.Now.Year - arrayGeboorteDatum[intTeller - 1].Year);
+
+                if (DateTime.Now.Month == arrayGeboorteDatum[intTeller - 1].Month
+                 && DateTime.Now.Day < arrayGeboorteDatum[intTeller - 1].Day
+                 || DateTime.Now.Month < arrayGeboorteDatum[intTeller - 1].Month)
+                {
+                    intLeefTijd--;
+                }
+
+                if (intLeefTijd < 12)
+                {
+                    dblKinderBijslag += 150;
+                    
+                }
+
+                else if (12 <= intLeefTijd && intLeefTijd < 18)
+                {
+                    dblKinderBijslag += 235;
+                   
+                }
+            }
+
+            if (intAantal == 3 || intAantal == 4)
+            {
+                dblKinderBijslag += dblKinderBijslag * 2 / 100;
+            }
+
+            else if (intAantal == 5)
+            {
+                dblKinderBijslag += dblKinderBijslag * 3 / 100;
+
+            }
+
+            else if (intAantal > 5)
+            {
+                dblKinderBijslag += dblKinderBijslag * 3.5 / 100;
+
+            }
+
+            tbKinderBijslag.Text = Convert.ToString(dblKinderBijslag);
+        }
+    
+
+
+
+        private void btnInvoeren_Click(object sender, EventArgs e)
+        {
+            arrayGeboorteDatum[intAantal] = Convert.ToDateTime(tbGeboorteDatum.Text);
+            tbGeboorteDatum.Clear();
+            intAantal++;
+            lblAantal.Text = intAantal.ToString();
+        }
+    }
+}

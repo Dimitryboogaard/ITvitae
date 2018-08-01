cf678973e3bc135f242a7464b07fd6b27884e94b
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-08-01 15:20:18 +0200

    Add files via upload
    Waterverbruik

---
+++ 8 Waterverbruik/Waterverbruik/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Waterverbruik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbTarief1 = new System.Windows.Forms.RadioButton();
+            this.rbTarief2 = new System.Windows.Forms.RadioButton();
+            this.rbTarief0 = new System.Windows.Forms.RadioButton();
+            this.tbVerbruik = new System.Windows.Forms.TextBox();
+            this.lblVerbruik = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnPrijs = new System.Windows.Forms.Button();
+            this.tbPrijs = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbTarief1
+            // 
+            this.rbTarief1.AutoSize = true;
+            this.rbTarief1.Location = new System.Drawing.Point(5, 4);
+            this.rbTarief1.Name = "rbTarief1";
+            this.rbTarief1.Size = new System.Drawing.Size(61, 17);
+            this.rbTarief1.TabIndex = 0;
+            this.rbTarief1.TabStop = true;
+            this.rbTarief1.Text = "Tarief 1";
+            this.rbTarief1.UseVisualStyleBackColor = true;
+            // 
+            // rbTarief2
+            // 
+            this.rbTarief2.AutoSize = true;
+            this.rbTarief2.Location = new System.Drawing.Point(5, 27);
+            this.rbTarief2.Name = "rbTarief2";
+            this.rbTarief2.Size = new System.Drawing.Size(61, 17);
+            this.rbTarief2.TabIndex = 1;
+            this.rbTarief2.TabStop = true;
+            this.rbTarief2.Text = "Tarief 2";
+            this.rbTarief2.UseVisualStyleBackColor = true;
+            // 
+            // rbTarief0
+            // 
+            this.rbTarief0.AutoSize = true;
+            this.rbTarief0.Location = new System.Drawing.Point(5, 50);
+            this.rbTarief0.Name = "rbTarief0";
+            this.rbTarief0.Size = new System.Drawing.Size(61, 17);
+            this.rbTarief0.TabIndex = 2;
+            this.rbTarief0.TabStop = true;
+            this.rbTarief0.Text = "Tarief 0";
+            this.rbTarief0.UseVisualStyleBackColor = true;
+            // 
+            // tbVerbruik
+            // 
+            this.tbVerbruik.Location = new System.Drawing.Point(17, 114);
+            this.tbVerbruik.Name = "tbVerbruik";
+            this.tbVerbruik.Size = new System.Drawing.Size(61, 20);
+            this.tbVerbruik.TabIndex = 3;
+            // 
+            // lblVerbruik
+            // 
+            this.lblVerbruik.AutoSize = true;
+            this.lblVerbruik.Location = new System.Drawing.Point(84, 117);
+            this.lblVerbruik.Name = "lblVerbruik";
+            this.lblVerbruik.Size = new System.Drawing.Size(69, 13);
+            this.lblVerbruik.TabIndex = 4;
+            this.lblVerbruik.Text = "Verbruik (m3)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbTarief0);
+            this.groupBox1.Controls.Add(this.rbTarief2);
+            this.groupBox1.Controls.Add(this.rbTarief1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(86, 93);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnPrijs
+            // 
+            this.btnPrijs.Location = new System.Drawing.Point(17, 140);
+            this.btnPrijs.Name = "btnPrijs";
+            this.btnPrijs.Size = new System.Drawing.Size(61, 21);
+            this.btnPrijs.TabIndex = 6;
+            this.btnPrijs.Text = "Prijs";
+            this.btnPrijs.UseVisualStyleBackColor = true;
+            this.btnPrijs.Click += new System.EventHandler(this.btnPrijs_Click);
+            // 
+            // tbPrijs
+            // 
+            this.tbPrijs.Location = new System.Drawing.Point(18, 182);
+            this.tbPrijs.Name = "tbPrijs";
+            this.tbPrijs.Size = new System.Drawing.Size(60, 20);
+            this.tbPrijs.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 244);
+            this.Controls.Add(this.tbPrijs);
+            this.Controls.Add(this.btnPrijs);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblVerbruik);
+            this.Controls.Add(this.tbVerbruik);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbTarief1;
+        private System.Windows.Forms.RadioButton rbTarief2;
+        private System.Windows.Forms.RadioButton rbTarief0;
+        private System.Windows.Forms.TextBox tbVerbruik;
+        private System.Windows.Forms.Label lblVerbruik;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnPrijs;
+        private System.Windows.Forms.TextBox tbPrijs;
+    }
+}
+
---
+++ 8 Waterverbruik/Waterverbruik/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Waterverbruik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Declareren van de variablen
+        const double cdblVastrechtTarief1 = 100, cdblVastrechtTarief2 = 75,
+                     cdblVerbruiksKostenTarief1 = 0.25, cdblVerbruiksKostenTarief2 = 0.38;
+        double dblVerbruik, dblPrijs;
+
+        private void btnPrijs_Click(object sender, EventArgs e)
+        {
+            dblVerbruik = Convert.ToDouble(tbVerbruik.Text);
+
+            if(rbTarief1.Checked)
+            {
+                dblPrijs = cdblVastrechtTarief1 + dblVerbruik * cdblVerbruiksKostenTarief1;
+                tbPrijs.Text = Convert.ToString(dblPrijs);
+            }
+
+            if(rbTarief2.Checked)
+            {
+                dblPrijs = cdblVastrechtTarief2 + dblVerbruik * cdblVerbruiksKostenTarief2;
+                tbPrijs.Text = Convert.ToString(dblPrijs);
+
+            }
+
+            if(rbTarief0.Checked)
+            {
+                if((cdblVerbruiksKostenTarief2 - cdblVerbruiksKostenTarief1) * dblVerbruik >
+                    cdblVastrechtTarief1 - cdblVastrechtTarief2)
+                {
+                    dblPrijs = cdblVastrechtTarief1 + dblVerbruik * cdblVerbruiksKostenTarief1;
+                    tbPrijs.Text = Convert.ToString(dblPrijs);
+
+                }
+
+                else
+                {
+                    dblPrijs = cdblVastrechtTarief2 + dblVerbruik * cdblVerbruiksKostenTarief2;
+                    tbPrijs.Text = Convert.ToString(dblPrijs);
+
+                }
+
+            }
+        }
+    }
+}

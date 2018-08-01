63d990f305179bec6d7c3be05206be0469111c65
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-08-01 15:18:19 +0200

    Add files via upload
    Houthandel

---
+++ 10 Houthandel/Houthandel/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Houthandel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbAantalM3Klasse1 = new System.Windows.Forms.TextBox();
+            this.tbAantalM2Schaven = new System.Windows.Forms.TextBox();
+            this.lblAantalM3Klasse1 = new System.Windows.Forms.Label();
+            this.lblAantalM2Schaven = new System.Windows.Forms.Label();
+            this.tbLeverDatum = new System.Windows.Forms.TextBox();
+            this.lblLeverDatum = new System.Windows.Forms.Label();
+            this.tbPrijs = new System.Windows.Forms.TextBox();
+            this.lblPrijs = new System.Windows.Forms.Label();
+            this.btnPrijs = new System.Windows.Forms.Button();
+            this.tbAantalM3Klasse2 = new System.Windows.Forms.TextBox();
+            this.tbAantalM3Klasse3 = new System.Windows.Forms.TextBox();
+            this.lblAantalM3Klasse2 = new System.Windows.Forms.Label();
+            this.lblAantalM3Klasse3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbAantalM3Klasse1
+            // 
+            this.tbAantalM3Klasse1.Location = new System.Drawing.Point(111, 12);
+            this.tbAantalM3Klasse1.Name = "tbAantalM3Klasse1";
+            this.tbAantalM3Klasse1.Size = new System.Drawing.Size(100, 20);
+            this.tbAantalM3Klasse1.TabIndex = 0;
+            this.tbAantalM3Klasse1.Text = "0";
+            this.tbAantalM3Klasse1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbAantalM2Schaven
+            // 
+            this.tbAantalM2Schaven.Location = new System.Drawing.Point(111, 90);
+            this.tbAantalM2Schaven.Name = "tbAantalM2Schaven";
+            this.tbAantalM2Schaven.Size = new System.Drawing.Size(100, 20);
+            this.tbAantalM2Schaven.TabIndex = 1;
+            this.tbAantalM2Schaven.Text = "0";
+            this.tbAantalM2Schaven.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblAantalM3Klasse1
+            // 
+            this.lblAantalM3Klasse1.AutoSize = true;
+            this.lblAantalM3Klasse1.Location = new System.Drawing.Point(9, 15);
+            this.lblAantalM3Klasse1.Name = "lblAantalM3Klasse1";
+            this.lblAantalM3Klasse1.Size = new System.Drawing.Size(96, 13);
+            this.lblAantalM3Klasse1.TabIndex = 2;
+            this.lblAantalM3Klasse1.Text = "Aantal m3 klasse 1";
+            // 
+            // lblAantalM2Schaven
+            // 
+            this.lblAantalM2Schaven.AutoSize = true;
+            this.lblAantalM2Schaven.Location = new System.Drawing.Point(7, 93);
+            this.lblAantalM2Schaven.Name = "lblAantalM2Schaven";
+            this.lblAantalM2Schaven.Size = new System.Drawing.Size(98, 13);
+            this.lblAantalM2Schaven.TabIndex = 3;
+            this.lblAantalM2Schaven.Text = "Aantal m2 schaven";
+            // 
+            // tbLeverDatum
+            // 
+            this.tbLeverDatum.Location = new System.Drawing.Point(111, 116);
+            this.tbLeverDatum.Name = "tbLeverDatum";
+            this.tbLeverDatum.Size = new System.Drawing.Size(100, 20);
+            this.tbLeverDatum.TabIndex = 4;
+            // 
+            // lblLeverDatum
+            // 
+            this.lblLeverDatum.AutoSize = true;
+            this.lblLeverDatum.Location = new System.Drawing.Point(39, 119);
+            this.lblLeverDatum.Name = "lblLeverDatum";
+            this.lblLeverDatum.Size = new System.Drawing.Size(66, 13);
+            this.lblLeverDatum.TabIndex = 5;
+            this.lblLeverDatum.Text = "Lever datum";
+            // 
+            // tbPrijs
+            // 
+            this.tbPrijs.Location = new System.Drawing.Point(111, 206);
+            this.tbPrijs.Name = "tbPrijs";
+            this.tbPrijs.Size = new System.Drawing.Size(100, 20);
+            this.tbPrijs.TabIndex = 6;
+            // 
+            // lblPrijs
+            // 
+            this.lblPrijs.AutoSize = true;
+            this.lblPrijs.Location = new System.Drawing.Point(79, 209);
+            this.lblPrijs.Name = "lblPrijs";
+            this.lblPrijs.Size = new System.Drawing.Size(26, 13);
+            this.lblPrijs.TabIndex = 7;
+            this.lblPrijs.Text = "Prijs";
+            // 
+            // btnPrijs
+            // 
+            this.btnPrijs.Location = new System.Drawing.Point(122, 177);
+            this.btnPrijs.Name = "btnPrijs";
+            this.btnPrijs.Size = new System.Drawing.Size(75, 23);
+            this.btnPrijs.TabIndex = 8;
+            this.btnPrijs.Text = "Prijs";
+            this.btnPrijs.UseVisualStyleBackColor = true;
+            this.btnPrijs.Click += new System.EventHandler(this.btnPrijs_Click);
+            // 
+            // tbAantalM3Klasse2
+            // 
+            this.tbAantalM3Klasse2.Location = new System.Drawing.Point(111, 38);
+            this.tbAantalM3Klasse2.Name = "tbAantalM3Klasse2";
+            this.tbAantalM3Klasse2.Size = new System.Drawing.Size(100, 20);
+            this.tbAantalM3Klasse2.TabIndex = 9;
+            this.tbAantalM3Klasse2.Text = "0";
+            this.tbAantalM3Klasse2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbAantalM3Klasse3
+            // 
+            this.tbAantalM3Klasse3.Location = new System.Drawing.Point(111, 64);
+            this.tbAantalM3Klasse3.Name = "tbAantalM3Klasse3";
+            this.tbAantalM3Klasse3.Size = new System.Drawing.Size(100, 20);
+            this.tbAantalM3Klasse3.TabIndex = 10;
+            this.tbAantalM3Klasse3.Text = "0";
+            this.tbAantalM3Klasse3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblAantalM3Klasse2
+            // 
+            this.lblAantalM3Klasse2.AutoSize = true;
+            this.lblAantalM3Klasse2.Location = new System.Drawing.Point(9, 41);
+            this.lblAantalM3Klasse2.Name = "lblAantalM3Klasse2";
+            this.lblAantalM3Klasse2.Size = new System.Drawing.Size(96, 13);
+            this.lblAantalM3Klasse2.TabIndex = 11;
+            this.lblAantalM3Klasse2.Text = "Aantal m3 klasse 2";
+            // 
+            // lblAantalM3Klasse3
+            // 
+            this.lblAantalM3Klasse3.AutoSize = true;
+            this.lblAantalM3Klasse3.Location = new System.Drawing.Point(9, 67);
+            this.lblAantalM3Klasse3.Name = "lblAantalM3Klasse3";
+            this.lblAantalM3Klasse3.Size = new System.Drawing.Size(96, 13);
+            this.lblAantalM3Klasse3.TabIndex = 12;
+            this.lblAantalM3Klasse3.Text = "Aantal m3 klasse 3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 288);
+            this.Controls.Add(this.lblAantalM3Klasse3);
+            this.Controls.Add(this.lblAantalM3Klasse2);
+            this.Controls.Add(this.tbAantalM3Klasse3);
+            this.Controls.Add(this.tbAantalM3Klasse2);
+            this.Controls.Add(this.btnPrijs);
+            this.Controls.Add(this.lblPrijs);
+            this.Controls.Add(this.tbPrijs);
+            this.Controls.Add(this.lblLeverDatum);
+            this.Controls.Add(this.tbLeverDatum);
+            this.Controls.Add(this.lblAantalM2Schaven);
+            this.Controls.Add(this.lblAantalM3Klasse1);
+            this.Controls.Add(this.tbAantalM2Schaven);
+            this.Controls.Add(this.tbAantalM3Klasse1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbAantalM3Klasse1;
+        private System.Windows.Forms.TextBox tbAantalM2Schaven;
+        private System.Windows.Forms.Label lblAantalM3Klasse1;
+        private System.Windows.Forms.Label lblAantalM2Schaven;
+        private System.Windows.Forms.TextBox tbLeverDatum;
+        private System.Windows.Forms.Label lblLeverDatum;
+        private System.Windows.Forms.TextBox tbPrijs;
+        private System.Windows.Forms.Label lblPrijs;
+        private System.Windows.Forms.Button btnPrijs;
+        private System.Windows.Forms.TextBox tbAantalM3Klasse2;
+        private System.Windows.Forms.TextBox tbAantalM3Klasse3;
+        private System.Windows.Forms.Label lblAantalM3Klasse2;
+        private System.Windows.Forms.Label lblAantalM3Klasse3;
+    }
+}
+
---
+++ 10 Houthandel/Houthandel/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Houthandel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Declareren van de variabelen
+        DateTime dteLeverDatum;
+        const double cdblSchaafLoon = 0.6, cdblKlasse1Prijs = 300, cdblKlasse2Prijs = 500,
+                     cdblKlasse3Prijs = 450, cdbl14Tot21Korting = 1, cdbl21tot28Korting = 2,
+                     cdbl28Korting = 2.5, cdblTotaleHoutkostenKorting = 200;
+        double dblAantalM3Klasse1, dblAantalM3Klasse2, dblAantalM3Klasse3, dblPrijs,
+               dblLeverTijdKorting, dblAantalM2Schaven;
+        int intAantalDagen;
+
+        private void btnPrijs_Click(object sender, EventArgs e)
+        {
+            dteLeverDatum = Convert.ToDateTime(tbLeverDatum.Text);
+            intAantalDagen = dteLeverDatum.DayOfYear - DateTime.Now.DayOfYear;
+
+            dblAantalM3Klasse1 = Convert.ToDouble(tbAantalM3Klasse1.Text);
+            dblAantalM3Klasse2 = Convert.ToDouble(tbAantalM3Klasse2.Text);
+            dblAantalM3Klasse3 = Convert.ToDouble(tbAantalM3Klasse3.Text);
+
+            dblAantalM2Schaven = Convert.ToDouble(tbAantalM2Schaven.Text);
+
+            //Levertijd korting bepalen
+
+            if(14 <= intAantalDagen && intAantalDagen < 21)
+            {
+                dblLeverTijdKorting = cdbl14Tot21Korting;
+            }
+
+            else if(21 <= intAantalDagen && intAantalDagen < 28)
+            {
+                dblLeverTijdKorting = cdbl21tot28Korting;
+            }
+
+            else if(28 <= intAantalDagen)
+            {
+                dblLeverTijdKorting = cdbl28Korting;
+            }
+
+            //Bepalen of er schaafkosten betaald moeten worden
+
+            if(dblAantalM3Klasse1 * cdblKlasse1Prijs +
+               dblAantalM3Klasse2 * cdblKlasse2Prijs +
+               dblAantalM3Klasse3 * cdblKlasse3Prijs < 200)
+            {
+                dblPrijs = dblAantalM3Klasse1 * cdblKlasse1Prijs +
+                           dblAantalM3Klasse2 * cdblKlasse2Prijs +
+                           dblAantalM3Klasse3 * cdblKlasse3Prijs +
+                           dblAantalM2Schaven * cdblSchaafLoon -
+                           (dblAantalM3Klasse1 * cdblKlasse1Prijs +
+                           dblAantalM3Klasse2 * cdblKlasse2Prijs +
+                           dblAantalM3Klasse3 * cdblKlasse3Prijs +
+                           dblAantalM2Schaven * cdblSchaafLoon) * dblLeverTijdKorting / 100;
+
+
+            }
+
+            else
+            {
+                dblPrijs = dblAantalM3Klasse1 * cdblKlasse1Prijs +
+                           dblAantalM3Klasse2 * cdblKlasse2Prijs +
+                           dblAantalM3Klasse3 * cdblKlasse3Prijs -
+                           (dblAantalM3Klasse1 * cdblKlasse1Prijs +
+                            dblAantalM3Klasse2 * cdblKlasse2Prijs +
+                            dblAantalM3Klasse3 * cdblKlasse3Prijs) * dblLeverTijdKorting / 100;
+
+            }
+
+            tbPrijs.Text = Convert.ToString(dblPrijs);
+        }
+    }
+}

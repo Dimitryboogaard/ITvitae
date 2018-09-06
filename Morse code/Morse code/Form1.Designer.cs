3f11cb5339fb0600e974e14a23463f111b61fe09
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-09-06 14:55:05 +0200

    Add files via upload

---
+++ Morse code/Morse code/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Morse_code
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMaakMorseCode = new System.Windows.Forms.Button();
+            this.rtTekst = new System.Windows.Forms.RichTextBox();
+            this.btnMaakGewoneTekst = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMaakMorseCode
+            // 
+            this.btnMaakMorseCode.Location = new System.Drawing.Point(24, 224);
+            this.btnMaakMorseCode.Name = "btnMaakMorseCode";
+            this.btnMaakMorseCode.Size = new System.Drawing.Size(131, 23);
+            this.btnMaakMorseCode.TabIndex = 0;
+            this.btnMaakMorseCode.Text = "Maak Morse code";
+            this.btnMaakMorseCode.UseVisualStyleBackColor = true;
+            this.btnMaakMorseCode.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtTekst
+            // 
+            this.rtTekst.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtTekst.Location = new System.Drawing.Point(24, 12);
+            this.rtTekst.Name = "rtTekst";
+            this.rtTekst.Size = new System.Drawing.Size(281, 186);
+            this.rtTekst.TabIndex = 1;
+            this.rtTekst.Text = "";
+            // 
+            // btnMaakGewoneTekst
+            // 
+            this.btnMaakGewoneTekst.Location = new System.Drawing.Point(168, 224);
+            this.btnMaakGewoneTekst.Name = "btnMaakGewoneTekst";
+            this.btnMaakGewoneTekst.Size = new System.Drawing.Size(137, 23);
+            this.btnMaakGewoneTekst.TabIndex = 2;
+            this.btnMaakGewoneTekst.Text = "Maak gewone tekst";
+            this.btnMaakGewoneTekst.UseVisualStyleBackColor = true;
+            this.btnMaakGewoneTekst.Click += new System.EventHandler(this.btnMaakGewoneTekst_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 267);
+            this.Controls.Add(this.btnMaakGewoneTekst);
+            this.Controls.Add(this.rtTekst);
+            this.Controls.Add(this.btnMaakMorseCode);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMaakMorseCode;
+        private System.Windows.Forms.RichTextBox rtTekst;
+        private System.Windows.Forms.Button btnMaakGewoneTekst;
+    }
+}
+
---
+++ Morse code/Morse code/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Morse_code
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            arrayMorseCode[33] = "-·-·--";
+            arrayMorseCode[38] = "·-···";
+            arrayMorseCode[39] = "·----·";
+            arrayMorseCode[40] = "-·--·";
+            arrayMorseCode[41] = "-·--·-";
+            arrayMorseCode[44] = "--··--";
+            arrayMorseCode[46] = "·-·-·-";
+            arrayMorseCode[47] = "-··-·";
+            arrayMorseCode[48] = "-----";
+            arrayMorseCode[49] = "·----";
+            arrayMorseCode[50] = "··---";
+            arrayMorseCode[51] = "···--";
+            arrayMorseCode[52] = "····-";
+            arrayMorseCode[53] = "·····";
+            arrayMorseCode[54] = "-····";
+            arrayMorseCode[55] = "--···";
+            arrayMorseCode[56] = "---··";
+            arrayMorseCode[57] = "----·";
+            arrayMorseCode[58] = "---···";
+            arrayMorseCode[59] = "-·-·-";
+            arrayMorseCode[61] = "-···-";
+            arrayMorseCode[63] = "··--··";
+            arrayMorseCode[64] = "·--·-·";
+            arrayMorseCode[65] = "·-";
+            arrayMorseCode[66] = "-···";
+            arrayMorseCode[67] = "-·-·";
+            arrayMorseCode[68] = "-··";
+            arrayMorseCode[69] = "·";
+            arrayMorseCode[70] = "··-·";
+            arrayMorseCode[71] = "--·";
+            arrayMorseCode[72] = "····";
+            arrayMorseCode[73] = "··";
+            arrayMorseCode[74] = "·---";
+            arrayMorseCode[75] = "-·-";
+            arrayMorseCode[76] = "·-··";
+            arrayMorseCode[77] = "--";
+            arrayMorseCode[78] = "-·";
+            arrayMorseCode[79] = "---";
+            arrayMorseCode[80] = "·--·";
+            arrayMorseCode[81] = "--·-";
+            arrayMorseCode[82] = "·-·";
+            arrayMorseCode[83] = "···";
+            arrayMorseCode[84] = "-";
+            arrayMorseCode[85] = "··-";
+            arrayMorseCode[86] = "·--";
+            arrayMorseCode[87] = "···-";
+            arrayMorseCode[88] = "-··-";
+            arrayMorseCode[89] = "-·--";
+            arrayMorseCode[90] = "--··";
+
+        }
+
+        string[] arrayMorseCode = new string[256];
+        string strTekst, strMorseCode, strSpatie;
+        int intASCII, intTeller, intTeller2, intStringLengte;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            strTekst = rtTekst.Text;
+
+            rtTekst.Clear();
+
+            foreach(char Karakter in strTekst)
+            {
+                intASCII = Convert.ToInt32(Karakter);
+                rtTekst.Text += arrayMorseCode[intASCII] + " ";
+                if(intASCII == 32)
+                {
+                    rtTekst.Text += "   ";
+                }
+
+            }
+        }
+
+        private void btnMaakGewoneTekst_Click(object sender, EventArgs e)
+        {
+            strTekst = rtTekst.Text;
+
+            rtTekst.Clear();
+
+            intStringLengte = strTekst.Length;
+
+            strMorseCode = "";
+
+            for(intTeller = 0; intTeller <= intStringLengte - 1; intTeller++)
+            {
+                if (strTekst.Substring(intTeller, 1) != " ")
+                {
+                    strMorseCode += strTekst.Substring(intTeller, 1);
+                }
+
+
+                if (strTekst.Substring(intTeller, 1) == " " || intTeller == intStringLengte - 1)
+                {
+                    strSpatie += " ";
+
+                    if(strSpatie == "   ")
+                    {
+                        rtTekst.Text += " ";
+                        strSpatie = ""; 
+                    }
+
+
+                        for (intTeller2 = 33; intTeller2 <= 90; intTeller2++)
+                        {
+                            if (strMorseCode == arrayMorseCode[intTeller2])
+                            {
+                                char Karakter = Convert.ToChar(intTeller2);
+                                rtTekst.Text += Karakter;
+                                strMorseCode = "";
+                            }
+
+                        }
+                    
+
+                }
+
+                if(strTekst.Substring(intTeller, 1) != " ")
+                {
+                    strSpatie = "";
+                }
+
+            }
+        }
+    }
+}
+

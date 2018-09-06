74e2a9aff43a882603c79d8b6f58d3f1d2f8505e
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-09-06 14:56:04 +0200

    Add files via upload

---
+++ Torens van Hanoi/Torens van Hanoi/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Torens_van_Hanoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtLinks = new System.Windows.Forms.RichTextBox();
+            this.rtMidden = new System.Windows.Forms.RichTextBox();
+            this.rtRechts = new System.Windows.Forms.RichTextBox();
+            this.btnLinks = new System.Windows.Forms.Button();
+            this.btnMidden = new System.Windows.Forms.Button();
+            this.btnRechts = new System.Windows.Forms.Button();
+            this.lblCommentaar = new System.Windows.Forms.Label();
+            this.btnOpnieuw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtLinks
+            // 
+            this.rtLinks.Enabled = false;
+            this.rtLinks.Location = new System.Drawing.Point(91, 70);
+            this.rtLinks.Name = "rtLinks";
+            this.rtLinks.Size = new System.Drawing.Size(23, 122);
+            this.rtLinks.TabIndex = 0;
+            this.rtLinks.Text = "";
+            // 
+            // rtMidden
+            // 
+            this.rtMidden.Enabled = false;
+            this.rtMidden.Location = new System.Drawing.Point(165, 70);
+            this.rtMidden.Name = "rtMidden";
+            this.rtMidden.Size = new System.Drawing.Size(23, 122);
+            this.rtMidden.TabIndex = 1;
+            this.rtMidden.Text = "";
+            // 
+            // rtRechts
+            // 
+            this.rtRechts.Enabled = false;
+            this.rtRechts.Location = new System.Drawing.Point(244, 70);
+            this.rtRechts.Name = "rtRechts";
+            this.rtRechts.Size = new System.Drawing.Size(23, 122);
+            this.rtRechts.TabIndex = 2;
+            this.rtRechts.Text = "";
+            // 
+            // btnLinks
+            // 
+            this.btnLinks.Location = new System.Drawing.Point(91, 198);
+            this.btnLinks.Name = "btnLinks";
+            this.btnLinks.Size = new System.Drawing.Size(24, 24);
+            this.btnLinks.TabIndex = 3;
+            this.btnLinks.UseVisualStyleBackColor = true;
+            this.btnLinks.Click += new System.EventHandler(this.btnLinks_Click);
+            // 
+            // btnMidden
+            // 
+            this.btnMidden.Location = new System.Drawing.Point(165, 198);
+            this.btnMidden.Name = "btnMidden";
+            this.btnMidden.Size = new System.Drawing.Size(24, 24);
+            this.btnMidden.TabIndex = 4;
+            this.btnMidden.UseVisualStyleBackColor = true;
+            this.btnMidden.Click += new System.EventHandler(this.btnMidden_Click);
+            // 
+            // btnRechts
+            // 
+            this.btnRechts.Location = new System.Drawing.Point(244, 198);
+            this.btnRechts.Name = "btnRechts";
+            this.btnRechts.Size = new System.Drawing.Size(24, 24);
+            this.btnRechts.TabIndex = 5;
+            this.btnRechts.UseVisualStyleBackColor = true;
+            this.btnRechts.Click += new System.EventHandler(this.btnRechts_Click_1);
+            // 
+            // lblCommentaar
+            // 
+            this.lblCommentaar.AutoSize = true;
+            this.lblCommentaar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCommentaar.Location = new System.Drawing.Point(61, 35);
+            this.lblCommentaar.Name = "lblCommentaar";
+            this.lblCommentaar.Size = new System.Drawing.Size(0, 19);
+            this.lblCommentaar.TabIndex = 7;
+            // 
+            // btnOpnieuw
+            // 
+            this.btnOpnieuw.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpnieuw.Location = new System.Drawing.Point(109, 249);
+            this.btnOpnieuw.Name = "btnOpnieuw";
+            this.btnOpnieuw.Size = new System.Drawing.Size(130, 37);
+            this.btnOpnieuw.TabIndex = 8;
+            this.btnOpnieuw.Text = "Opnieuw";
+            this.btnOpnieuw.UseVisualStyleBackColor = true;
+            this.btnOpnieuw.Visible = false;
+            this.btnOpnieuw.Click += new System.EventHandler(this.btnOpnieuw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 307);
+            this.Controls.Add(this.btnOpnieuw);
+            this.Controls.Add(this.lblCommentaar);
+            this.Controls.Add(this.btnRechts);
+            this.Controls.Add(this.btnMidden);
+            this.Controls.Add(this.btnLinks);
+            this.Controls.Add(this.rtRechts);
+            this.Controls.Add(this.rtMidden);
+            this.Controls.Add(this.rtLinks);
+            this.Name = "Form1";
+            this.Text = "Torens van Hanoi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtLinks;
+        private System.Windows.Forms.RichTextBox rtMidden;
+        private System.Windows.Forms.RichTextBox rtRechts;
+        private System.Windows.Forms.Button btnLinks;
+        private System.Windows.Forms.Button btnMidden;
+        private System.Windows.Forms.Button btnRechts;
+        private System.Windows.Forms.Label lblCommentaar;
+        private System.Windows.Forms.Button btnOpnieuw;
+    }
+}
+
---
+++ Torens van Hanoi/Torens van Hanoi/Form1.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Torens_van_Hanoi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            arraySpel[1, 1] = 1;
+            arraySpel[1, 2] = 2;
+            arraySpel[1, 3] = 3;
+            arraySpel[1, 4] = 4;
+            arraySpel[1, 5] = 5;
+            arraySpel[1, 6] = 6;
+            arraySpel[1, 7] = 7;
+            arraySpel[1, 8] = 8;
+            arraySpel[1, 9] = 9;
+            arraySpel[2, 9] = 9;
+            arraySpel[3, 9] = 9;
+
+            for (intTeller = 1; intTeller <= 8; intTeller++)
+            {
+
+                rtLinks.Text += Convert.ToString(arraySpel[1, intTeller]) + Environment.NewLine;
+            }
+        }
+
+        int[,] arraySpel = new int[4, 10];
+        int intTeller, intVanRij, intNaarRij;
+        bool booGewonnen;
+
+        private void btnLinks_Click(object sender, EventArgs e)
+        {
+            if (btnMidden.Text == "" && btnRechts.Text == "" && btnLinks.Text == "")
+            {
+                btnLinks.Text = "x";
+            }
+
+            else if(btnLinks.Text == "x")
+            {
+                btnLinks.Text = "";
+            }
+
+            if (btnMidden.Text == "x")
+            {
+                VanRij2();
+                NaarRij1();
+
+                if (arraySpel[2, intVanRij] < arraySpel[1, intNaarRij])
+                {
+                    arraySpel[1, intNaarRij - 1] = arraySpel[2, intVanRij];
+                    arraySpel[2, intVanRij] = 0;
+
+                    lblCommentaar.Text = "";
+
+                    TekenTorens();
+                }
+
+                else
+                {
+                    lblCommentaar.Text = "Kan niet.";
+                    btnMidden.Text = "";
+                }
+
+            }
+
+            if (btnRechts.Text == "x")
+            {
+                VanRij3();
+                NaarRij1();
+
+                if (arraySpel[3, intVanRij] < arraySpel[1, intNaarRij])
+                {
+                    arraySpel[1, intNaarRij - 1] = arraySpel[3, intVanRij];
+                    arraySpel[3, intVanRij] = 0;
+
+                    lblCommentaar.Text = "";
+
+                    TekenTorens();
+                }
+
+                else
+                {
+                    lblCommentaar.Text = "Kan niet.";
+                    btnRechts.Text = "";
+                }
+
+            }
+
+
+        }
+
+        private void btnMidden_Click(object sender, EventArgs e)
+        {
+            if (btnLinks.Text == "" && btnRechts.Text == "" && btnMidden.Text == "")
+            {
+                btnMidden.Text = "x";
+            }
+
+            else if (btnMidden.Text == "x")
+            {
+                btnMidden.Text = "";
+            }
+
+            if (btnLinks.Text == "x")
+            {
+                VanRij1();
+                NaarRij2();
+
+                if (arraySpel[1, intVanRij] < arraySpel[2, intNaarRij])
+                {
+                    arraySpel[2, intNaarRij - 1] = arraySpel[1, intVanRij];
+                    arraySpel[1, intVanRij] = 0;
+
+                    lblCommentaar.Text = "";
+
+                    TekenTorens();
+                }
+
+                else
+                {
+                    lblCommentaar.Text = "Kan niet.";
+                    btnLinks.Text = "";
+                }
+            }
+
+            if (btnRechts.Text == "x")
+            {
+                VanRij3();
+                NaarRij2();
+
+                if (arraySpel[3, intVanRij] < arraySpel[2, intNaarRij])
+                {
+                    arraySpel[2, intNaarRij - 1] = arraySpel[3, intVanRij];
+                    arraySpel[3, intVanRij] = 0;
+
+                    lblCommentaar.Text = "";
+
+                    TekenTorens();
+
+                }
+
+                else
+                {
+                    lblCommentaar.Text = "Kan niet.";
+                    btnRechts.Text = "";
+                }
+
+
+            }
+        }
+
+        private void btnRechts_Click_1(object sender, EventArgs e)
+        {
+            if (btnLinks.Text == "" && btnMidden.Text == "" && btnRechts.Text == "")
+            {
+                btnRechts.Text = "x";
+            }
+
+            else if (btnRechts.Text == "x")
+            {
+                btnRechts.Text = "";
+            }
+
+
+            if (btnLinks.Text == "x")
+            {
+                VanRij1();
+                NaarRij3();
+
+                if (arraySpel[1, intVanRij] < arraySpel[3, intNaarRij])
+                {
+                    arraySpel[3, intNaarRij - 1] = arraySpel[1, intVanRij];
+                    arraySpel[1, intVanRij] = 0;
+
+                    lblCommentaar.Text = "";
+
+                    TekenTorens();
+                }
+
+                else
+                {
+                    lblCommentaar.Text = "Kan niet.";
+                    btnLinks.Text = "";
+                }
+            }
+
+            if (btnMidden.Text == "x")
+            {
+                VanRij2();
+                NaarRij3();
+
+                if (arraySpel[2, intVanRij] < arraySpel[3, intNaarRij])
+                {
+                    arraySpel[3, intNaarRij - 1] = arraySpel[2, intVanRij];
+                    arraySpel[2, intVanRij] = 0;
+
+                    lblCommentaar.Text = "";
+
+                    TekenTorens();
+                }
+
+                else
+                {
+                    lblCommentaar.Text = "Kan niet.";
+                    btnMidden.Text = "";
+                }
+            }
+
+
+
+        }
+
+        void VanRij1()
+        {
+            intVanRij = 1;
+            while (arraySpel[1, intVanRij] == 0 && intVanRij < 8)
+            {
+                intVanRij++;
+            } 
+
+
+            return;
+        }
+
+        void VanRij2()
+        {
+            intVanRij = 1;
+            while (arraySpel[2, intVanRij] == 0 && intVanRij < 8) 
+            {
+                intVanRij++;
+            } 
+
+            return;
+        }
+
+        void VanRij3()
+        {
+            intVanRij = 1;
+            while(arraySpel[3, intVanRij] == 0 && intVanRij < 8)
+            {
+                intVanRij++;
+            } 
+
+            return;
+        }
+
+        void NaarRij1()
+        {
+            intNaarRij = 1;
+            while (arraySpel[1, intNaarRij] == 0 && intNaarRij < 9) 
+            {
+                intNaarRij++;
+            } 
+
+            return;
+        }
+
+        
+
+        void NaarRij2()
+        {
+            intNaarRij = 1;
+            while (arraySpel[2, intNaarRij] == 0 && intNaarRij < 9)
+            {
+                intNaarRij++;
+            } 
+
+            return;
+        }
+
+
+        void NaarRij3()
+        {
+            intNaarRij = 1;
+            while (arraySpel[3, intNaarRij] == 0 && intNaarRij < 9)
+            {
+                intNaarRij++;
+            } 
+
+            return;
+        }
+
+
+        void TekenTorens()
+        {
+            btnLinks.Text = "";
+            btnMidden.Text = "";
+            btnRechts.Text = "";
+
+            rtLinks.Clear();
+            rtMidden.Clear();
+            rtRechts.Clear();
+
+            for(intTeller = 1; intTeller <= 8; intTeller++)
+            {
+                if(arraySpel[1, intTeller] == 0)
+                {
+                    rtLinks.Text += " " + Environment.NewLine;
+                }
+
+                else
+                {
+                    rtLinks.Text += Convert.ToString(arraySpel[1, intTeller]) + Environment.NewLine;
+                }
+
+                if (arraySpel[2, intTeller] == 0)
+                {
+                    rtMidden.Text += " " + Environment.NewLine;
+                }
+
+                else
+                {
+                    rtMidden.Text += Convert.ToString(arraySpel[2, intTeller]) + Environment.NewLine; ;
+                }
+
+                if (arraySpel[3, intTeller] == 0)
+                {
+                    rtRechts.Text += " " + Environment.NewLine;
+                }
+
+                else
+                {
+                    rtRechts.Text += Convert.ToString(arraySpel[3, intTeller]) + Environment.NewLine;
+                }
+
+
+            }
+
+            booGewonnen = true;
+
+            for(intTeller = 1; intTeller <= 8; intTeller++)
+            {
+                if(arraySpel[2, intTeller] == 0)
+                {
+                    booGewonnen = false;
+                }
+            }
+
+            if (!booGewonnen)
+            {
+                booGewonnen = true;
+
+                for (intTeller = 1; intTeller <= 8; intTeller++)
+                {
+                    if (arraySpel[3, intTeller] == 0)
+                    {
+                        booGewonnen = false;
+                    }
+                }
+            }
+
+            if(booGewonnen)
+            {
+                lblCommentaar.Text = "Gefeliciteerd!";
+
+                btnLinks.Enabled = false;
+                btnMidden.Enabled = false;
+                btnRechts.Enabled = false;
+
+                btnOpnieuw.Visible = true;
+            }
+
+            return;
+        }
+
+        private void btnOpnieuw_Click(object sender, EventArgs e)
+        {
+            arraySpel[1, 1] = 1;
+            arraySpel[1, 2] = 2;
+            arraySpel[1, 3] = 3;
+            arraySpel[1, 4] = 4;
+            arraySpel[1, 5] = 5;
+            arraySpel[1, 6] = 6;
+            arraySpel[1, 7] = 7;
+            arraySpel[1, 8] = 8;
+
+            for (intTeller = 1; intTeller <= 8; intTeller++)
+            {
+                arraySpel[2, intTeller] = 0;
+                arraySpel[3, intTeller] = 0;
+            }
+
+            TekenTorens();
+
+            btnLinks.Enabled = true;
+            btnMidden.Enabled = true;
+            btnRechts.Enabled = true;
+
+            btnOpnieuw.Visible = false;
+
+            lblCommentaar.Text = "";
+
+        }
+
+    }
+}

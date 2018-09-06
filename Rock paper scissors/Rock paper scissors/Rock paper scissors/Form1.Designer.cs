bac42a494fc1da4e873b02e5152cf5ecebab1498
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-09-06 14:59:28 +0200

    Add files via upload

---
+++ Rock paper scissors/Rock paper scissors/Rock paper scissors/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Rock_paper_scissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.lblWaardeComputer = new System.Windows.Forms.Label();
+            this.lblWinnaar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRock
+            // 
+            this.btnRock.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRock.Location = new System.Drawing.Point(67, 63);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(95, 35);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaper.Location = new System.Drawing.Point(67, 104);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(95, 35);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScissors.Location = new System.Drawing.Point(67, 145);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(95, 35);
+            this.btnScissors.TabIndex = 2;
+            this.btnScissors.Text = "Scissors";
+            this.btnScissors.UseVisualStyleBackColor = true;
+            this.btnScissors.Click += new System.EventHandler(this.btnScissors_Click);
+            // 
+            // lblWaardeComputer
+            // 
+            this.lblWaardeComputer.AutoSize = true;
+            this.lblWaardeComputer.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWaardeComputer.Location = new System.Drawing.Point(237, 109);
+            this.lblWaardeComputer.Name = "lblWaardeComputer";
+            this.lblWaardeComputer.Size = new System.Drawing.Size(0, 19);
+            this.lblWaardeComputer.TabIndex = 3;
+            // 
+            // lblWinnaar
+            // 
+            this.lblWinnaar.AutoSize = true;
+            this.lblWinnaar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinnaar.Location = new System.Drawing.Point(101, 19);
+            this.lblWinnaar.Name = "lblWinnaar";
+            this.lblWinnaar.Size = new System.Drawing.Size(0, 19);
+            this.lblWinnaar.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 301);
+            this.Controls.Add(this.lblWinnaar);
+            this.Controls.Add(this.lblWaardeComputer);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Label lblWaardeComputer;
+        private System.Windows.Forms.Label lblWinnaar;
+    }
+}
+
---
+++ Rock paper scissors/Rock paper scissors/Rock paper scissors/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rock_paper_scissors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            arrayWaarden[1] = "Rock";
+            arrayWaarden[2] = "Paper";
+            arrayWaarden[3] = "Scissors";
+        }
+
+        string[] arrayWaarden = new string[4];
+        Random rndWillekeurigGetal = new Random();
+        int intWaardeGebruiker, intWillekeurigGetal;
+
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            intWillekeurigGetal = rndWillekeurigGetal.Next(1, 4);
+            lblWaardeComputer.Text = arrayWaarden[intWillekeurigGetal];
+
+            if (intWillekeurigGetal == 1)
+            {
+                lblWinnaar.Text = "Gelijk";
+            }
+
+            else if (intWillekeurigGetal == 2)
+            {
+                lblWinnaar.Text = "Ik win";
+            }
+
+            else if (intWillekeurigGetal == 3)
+            {
+                lblWinnaar.Text = "U wint";
+            }
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            intWillekeurigGetal = rndWillekeurigGetal.Next(1, 4);
+            lblWaardeComputer.Text = arrayWaarden[intWillekeurigGetal];
+
+            if (intWillekeurigGetal == 1)
+            {
+                lblWinnaar.Text = "U wint";
+            }
+
+            else if (intWillekeurigGetal == 2)
+            {
+                lblWinnaar.Text = "Gelijk";
+            }
+
+            else if (intWillekeurigGetal == 3)
+            {
+                lblWinnaar.Text = "Ik win";
+            }
+
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            intWillekeurigGetal = rndWillekeurigGetal.Next(1, 4);
+            lblWaardeComputer.Text = arrayWaarden[intWillekeurigGetal];
+
+            if (intWillekeurigGetal == 1)
+            {
+                lblWinnaar.Text = "Ik win";
+            }
+
+            else if (intWillekeurigGetal == 2)
+            {
+                lblWinnaar.Text = "U wint";
+            }
+
+            else if (intWillekeurigGetal == 3)
+            {
+                lblWinnaar.Text = "Gelijk";
+            }
+
+        }
+
+    }
+}
